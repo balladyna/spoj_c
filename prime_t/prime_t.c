43e5967e2a74fbfee378f9f06a31eed1520e80c0
balladyna <91394225+balladyna@users.noreply.github.com>
2023-09-28 11:43:47 +0200

    Ex Prime_t

--- prime_t/prime_t.c
+++ prime_t/prime_t.c
@@ -1,51 +1,40 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-const char* is_prime();
+const char *is_prime();
 
-int main()
-{
-  int a, n;
+int main() {
+  int a, no_test;
 
+  scanf("%d\n", &no_test);
 
-  scanf("%d\n", &n);
-
-  for (a = 0; a < n; a++)
-  {
+  for (a = 0; a < no_test; a++) {
     printf(is_prime());
   }
   return 0;
 }
 
-const char*  is_prime()
-{
+const char *is_prime() {
   int i, b, z;
 
   b = 0;
 
   scanf("%d", &z);
-  if (z > 1)
-    {
-      if (z == 2)
-      {
-        return "TAK\n";
-      }
-      for (i = 2; i < (sqrt(z) + 1);)
-      {
-        if (z % i == 0)
-        {
-          b++;
-          return "NIE\n";
-        }
-        i++;
-      }
-      if (b == 0)
-        {
-          return "TAK\n";
-        }
+  if (z > 1) {
+    if (z == 2) {
+      return "TAK\n";
     }
-  else
-  {
+    for (i = 2; i < (sqrt(z) + 1);) {
+      if (z % i == 0) {
+        b++;
+        return "NIE\n";
+      }
+      i++;
+    }
+    if (b == 0) {
+      return "TAK\n";
+    }
+  } else {
     return "NIE\n";
   }
   return NULL;
